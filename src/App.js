79c874f9fd474877bdf3776f9795978cb6489aad
caro barreiro <42847079+carobarreirov@users.noreply.github.com>
2022-12-21 20:40:06 +0000

    first edit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,18 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Caro Barreiro doing <span className="heart">♥️</span> React
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+          Testing <code>react</code> Codespaces template.
         </p>
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://carobarreirov.github.io"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            Learn React
+            Portfolio
           </a>
         </p>
       </header>
